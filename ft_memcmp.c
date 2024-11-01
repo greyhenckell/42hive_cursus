42fc1f066267a2499cafdf3d9aa33f49c83f308b
Franck Henckell <fhenckel@c1r5p10.hive.fi>
2024-11-01 16:19:41 +0200

    mem cmp fix

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,30 +6,51 @@
 /*   By: fhenckel <fhenckel@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/29 10:38:08 by fhenckel          #+#    #+#             */
-/*   Updated: 2024/10/31 12:56:29 by fhenckel         ###   ########.fr       */
+/*   Updated: 2024/11/01 16:19:17 by fhenckel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+//#include <stdio.h>
+//#include <string.h>
 
 int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
-	char	*ps1;
-	char	*ps2;
-	int		i;
+	unsigned char	*ps1;
+	unsigned char	*ps2;
+	size_t	i;
+	int diff;
 
-	ps1 = (char *)s1;
-	ps2 = (char *)s2;
-	i = 0;
-	while ((i < (int)n) && (ps1[i] == ps2[i]))
+	if (n == 0)
+		return (0);
+
+	/*if (n >= 4)
+	{
+		diff =  *(int *)s1- *(int *)s2;
+		if (diff)
+			return diff;
+	}*/
+	ps1 = (unsigned char *)s1;
+	ps2 = (unsigned char *)s2;
+	i = 0 ;
+	while(i < n)
+	{
+		if (ps1[i] != ps2[i])
+			return ps1[i] - ps2[i];
 		i++;
-	return (ps1[i] - ps2[i]);
+	}
+	return 0;
 }
 /*
 int main()
 {
-	char e1[50] = "welcome to hive";
-	char e2[50] = "welcome to hive";
-	printf("%d\n", ft_memcmp(e1,e2,30));
-	printf("%d\n", memcmp(e1,e2,30));
+	char *e1 = "welcome ";
+	char *e2 = "welpole";
+	printf("%d\n", ft_memcmp(e1,e2,5));
+	printf("%d\n", memcmp(e1,e2,5));
+	printf("-------\n");
+	long setone[5] = { 1, 3, 5, 7, 9 };
+	long settwo[5] = { 3, 2, 5, 7, 8 };
+	printf("%d\n", memcmp(&setone, &settwo,5));
+	printf("%d\n", ft_memcmp(&setone, &settwo,5));
 }*/
