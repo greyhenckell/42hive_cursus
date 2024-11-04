42305cd8153c82de8c87729372b06d7b75998d9d
Franck Henckell <fhenckel@c1r6p12.hive.fi>
2024-11-04 19:18:09 +0200

    atoi fix space and normi

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: fhenckel <fhenckel@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/31 09:32:21 by fhenckel          #+#    #+#             */
-/*   Updated: 2024/10/31 09:49:45 by fhenckel         ###   ########.fr       */
+/*   Updated: 2024/11/04 19:17:47 by fhenckel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ int	ft_atoi(const char *nptr)
 	i = 0;
 	out = 0;
 	sign = 1;
-	while (p[i] == 32)
+	while ((p[i] >= 9 && p[i] <= 13) || p[i] == 32)
 		i++;
 	if ((p[i] == '+' || p[i] == '-'))
 	{
@@ -36,12 +36,12 @@ int	ft_atoi(const char *nptr)
 		out = (p[i] - '0') + (out * 10);
 		i++;
 	}
-	return (out * sign);
+	return ((int)(out * sign));
 }
 /*
 int main()
 {
 
-    printf("%d\n", ft_atoi("- 21474836489"));
-    printf("%d\n", atoi("- 21474836489"));
+    printf("%d\n", ft_atoi("2147483649"));
+    printf("%d\n", atoi("2147483649"));
 }*/
