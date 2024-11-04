3f37d3a27d22023750ddc11380d3f1fa70a2607d
Franck Henckell <fhenckel@c1r6p12.hive.fi>
2024-11-04 18:54:16 +0200

    lcat fixed and normi

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,21 +6,28 @@
 /*   By: fhenckel <fhenckel@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/31 10:32:04 by fhenckel          #+#    #+#             */
-/*   Updated: 2024/10/31 13:05:22 by fhenckel         ###   ########.fr       */
+/*   Updated: 2024/11/04 18:53:42 by fhenckel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
 //#include <bsd/string.h>
+//#include <stdio.h>
 
 size_t	ft_strlcat(char *dst, const char *src, size_t size)
 {
-	int	append_limit;
-	int	i;
-	int	dst_size;
+	int		append_limit;
+	int		i;
+	size_t	dst_size;
+	size_t	src_size;
 
 	i = 0;
+	if (!dst && size == 0)
+		return (ft_strlen(src));
 	dst_size = ft_strlen(dst);
+	src_size = ft_strlen(src);
 	append_limit = size - dst_size - 1;
+	if (dst_size > size)
+		return (size + src_size);
 	dst = dst + dst_size;
 	while (i < append_limit)
 	{
@@ -28,14 +35,21 @@ size_t	ft_strlcat(char *dst, const char *src, size_t size)
 		i++;
 	}
 	dst[i] = '\0';
-	return (dst_size + ft_strlen(src));
+	return (dst_size + src_size);
 }
 /*
 int main()
 {
-	char source[] = "_welcome";
-	char dest[] = "hola";
-	size_t r = strlcat(dest, source, strlen(source)+1);
+	char source[] = "ABC";
+	char dest[] = "";
+	printf("src_len:%lu, dst_len:%lu\n", strlen(source), strlen(dest));
+	size_t r = ft_strlcat(dest, source, 0);
 	printf("%s\n", dest);
 	printf("%zu\n", r);
+	printf("--------------\n");
+	char source2[] = "ABC";
+        char dest2[] = "";
+        size_t r2 = strlcat((void*)0, source2, 0);
+        printf("%s\n", dest2);
+        printf("%zu\n", r2);
 }*/
