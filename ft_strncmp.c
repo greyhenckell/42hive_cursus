c201d68b48fe8570a228b8090bba8b7bb6c18fb5
Franck Henckell <fhenckel@c1r6p12.hive.fi>
2024-11-04 10:41:46 +0200

    strncmp normi

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,27 +6,31 @@
 /*   By: fhenckel <fhenckel@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/29 17:48:21 by fhenckel          #+#    #+#             */
-/*   Updated: 2024/10/31 09:19:20 by fhenckel         ###   ########.fr       */
+/*   Updated: 2024/11/04 10:41:16 by fhenckel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_strncmp(const char *s1, const char *s2, unsigned int n)
 {
 	unsigned int	i;
-	char			*p1;
-	char			*p2;
+	unsigned char	*p1;
+	unsigned char	*p2;
 
-	p1 = (char *)s1;
-	p2 = (char *)s2;
+	p1 = (unsigned char *)s1;
+	p2 = (unsigned char *)s2;
 	i = 0;
-	while (i < n && p1[i] == p2[i] && (p1[i] != '\0' || p2[i] != '\0'))
+	while ((i < n) && (p1[i] != '\0' || p2[i] != '\0'))
+	{
+		if (p1[i] != p2[i])
+			return ((p1[i] > p2[i]) - (p2[i] > p1[i]));
 		i++;
-	return ((p1[i] > p2[i]) - (p2[i] > p1[i]));
+	}
+	return (0);
 }
 /*
 int main()
 {
-    printf("%d\n", ft_strncmp("hola", "", 6));
+    printf("%d\n", ft_strncmp("abcdef", "abcdefghijklmnop", 6));
     printf("---------\n");
-    printf("%d\n", strncmp("hola", "", 6));
+    printf("%d\n", strncmp("abcdef", "abcdefghijklmnop", 6));
 }*/
