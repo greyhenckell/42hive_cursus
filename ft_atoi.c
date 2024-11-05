b389b3dbb03c44c047c37272f95b823455d83353
Franck Henckell <fhenckel@c1r5p10.hive.fi>
2024-11-05 12:34:02 +0200

    atoi handle limits

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: fhenckel <fhenckel@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/31 09:32:21 by fhenckel          #+#    #+#             */
-/*   Updated: 2024/11/04 19:17:47 by fhenckel         ###   ########.fr       */
+/*   Updated: 2024/11/05 12:32:27 by fhenckel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,28 +15,28 @@
 //#include <stdlib.h>
 int	ft_atoi(const char *nptr)
 {
-	char	*p;
 	long	out;
 	int		i;
 	int		sign;
 
-	p = (char *)nptr;
 	i = 0;
 	out = 0;
 	sign = 1;
-	while ((p[i] >= 9 && p[i] <= 13) || p[i] == 32)
+	while ((nptr[i] >= 9 && nptr[i] <= 13) || nptr[i] == 32)
 		i++;
-	if ((p[i] == '+' || p[i] == '-'))
+	if ((nptr[i] == '+' || nptr[i] == '-'))
 	{
-		if (p[i] == '-')
+		if (nptr[i] == '-')
 			sign = -1;
 		i = i + 1;
 	}
-	while (p[i] >= '0' && p[i] <= '9')
+	while (nptr[i] >= '0' && nptr[i] <= '9')
 	{
-		if (out > (INT_MAX-(p[i]-48))/10 )
-			return (sign == 1?  INT_MAX: INT_MIN);
-		out = (out * 10) + p[i]-48;
+		out = (out * 10) + (nptr[i] - 48);
+		if (out < 0 && sign == -1)
+			return ((int)LONG_MIN);
+		else if (out < 0 && sign == 1)
+			return ((int)LONG_MAX);
 		i++;
 	}
 	return ((int)(out * sign));
@@ -44,7 +44,8 @@ int	ft_atoi(const char *nptr)
 /*
 int main()
 {
-
-    printf("%d\n", ft_atoi("4000000000"));
-    printf("%d\n", atoi("4000000000"));
+    printf("%d\n", ft_atoi("-4000000000"));
+    printf("%d\n", atoi("-4000000000"));
+    printf("%ld\n", strtol("-4000000000", 0,10));
+    printf("%d\n", (int)strtol("-4000000000",0,10));
 }*/
