7f9cd64d6c8534e9643d2bb60e1fc4136f9e921f
Franck Henckell <fhenckel@c1r5p10.hive.fi>
2024-11-05 15:07:11 +0200

    fix strnstr handleing null and comparing func

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: fhenckel <fhenckel@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/30 09:42:08 by fhenckel          #+#    #+#             */
-/*   Updated: 2024/10/31 11:42:27 by fhenckel         ###   ########.fr       */
+/*   Updated: 2024/11/05 15:06:17 by fhenckel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
@@ -16,21 +16,25 @@
 char	*ft_strnstr(const char *big, const char *little, size_t len)
 {
 	size_t	i;
-	char	*pbig;
-	char	*plit;
+	size_t lenlit;
 
-	pbig = (char *)big;
-	plit = (char *)little;
-	if (!(*pbig) || !len)
+	if (!big && len == 0)
 		return (NULL);
-	if (!(*plit))
-		return (pbig);
+
+	lenlit = ft_strlen(little);
+	if (lenlit == 0)
+		return (char *)(big);
 	i = 0;
-	while (pbig[i] != '\0' && i < len)
+	while (big[i] != '\0' && i < len)
 	{
-		if (pbig[i] == plit[0])
-			if (!(ft_strncmp(pbig + i, plit, ft_strlen(plit) - 1)))
-				return (pbig + i);
+		if (big[i] == little[0])
+			if (!(ft_strncmp(big + i, little, lenlit )))
+			{
+				i += lenlit;
+				if (i > len)
+				       return (NULL);
+				return (char *)(big + i-lenlit);
+			}
 		i++;
 	}
 	return (NULL);
@@ -38,8 +42,10 @@ char	*ft_strnstr(const char *big, const char *little, size_t len)
 /*
 int main()
 {
-	const char *big = "dfgdfgf";
-	const char *small = "fg";
-	printf("%s\n",ft_strnstr(big, small, 10 ));
-	printf("%s\n",strnstr(big, small, 10 ));
+	char *big = "oh no not the empty string !";
+	char *small = "";
+	//char *small = "";
+	printf("len:%lu\n", strlen(big));
+	printf("%s\n",ft_strnstr((void*)0, small, 0 ));
+	printf("%s\n",strnstr((void*)0, small, 0 ));
 }*/
