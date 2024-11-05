ec3deb525778eb30b366025732a12ff62d8f255e
Franck Henckell <fhenckel@c1r5p10.hive.fi>
2024-11-05 16:20:51 +0200

    calloc overflow and normi

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: fhenckel <fhenckel@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/31 09:36:36 by fhenckel          #+#    #+#             */
-/*   Updated: 2024/10/31 10:19:51 by fhenckel         ###   ########.fr       */
+/*   Updated: 2024/11/05 16:18:57 by fhenckel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,15 @@
 
 void	*ft_calloc(size_t nmemb, size_t size)
 {
-	long	nmemsize;
-	int		*p;
+	unsigned int	nmemsize;
+	int				*p;
 
-	if (nmemb == 0 || size == 0)
+	if (nmemb != 0 && size > (UINT_MAX / nmemb))
 		return (NULL);
-	nmemsize = nmemb * size;
+	if (!nmemb || !size)
+		nmemsize = 1;
+	else
+		nmemsize = nmemb * size;
 	p = malloc(nmemsize);
 	if (p == NULL)
 		return (NULL);
@@ -31,16 +34,17 @@ void	*ft_calloc(size_t nmemb, size_t size)
 /*
 int main()
 {
-    // printf("%ld, %ld", sizeof(char), sizeof(int));
-
     char s[] = "hola welcome";
     char *ptr;
 
     ptr = s;
 
     printf("%s\n", ptr);
-    ptr = (char *)ft_calloc(50000000, 500);
-
+    ptr = (char *)calloc(50000000000000, 10);
+    if (ptr == NULL)
+	printf("calloc not allocated\n");
+    else
+    	printf("ptr created\n");
     int i = 0;
     while (i < 20)
     {
