47c4eebe2adf1d25f9e73c712925308bedd54ca3
Franck Henckell <fhenckel@c1r6p15.hive.fi>
2024-11-07 15:02:24 +0200

    trim normi

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,40 +6,48 @@
 /*   By: fhenckel <fhenckel@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/05 18:01:50 by fhenckel          #+#    #+#             */
-/*   Updated: 2024/11/05 18:59:07 by fhenckel         ###   ########.fr       */
+/*   Updated: 2024/11/07 15:01:29 by fhenckel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-//#include <stdio.h>
+
 #include "libft.h"
 
-char *ft_strtrim(char const *s1, char const *set)
+static int	trim_begin(char const *s1, char const *set)
 {
-	char *out;
-	int i = 0;
-	size_t lenstr;
-   
-	lenstr = ft_strlen(s1);
+	int	i;
+
+	i = 0;
 	while (s1[i] != '\0')
-    	{
-		if(ft_strchr(set, s1[i]))
+	{
+		if (ft_strchr(set, s1[i]))
 			i++;
-        	else
-            		break;
-    	}
-    	s1 = s1 + i;
-    	while( lenstr-i > 0)
-    	{
-		if(ft_strchr(set, s1[lenstr-i-1]))
+		else
+			break ;
+	}
+	return (i);
+}
+
+char	*ft_strtrim(char const *s1, char const *set)
+{
+	char	*out;
+	int		i;
+	size_t	lenstr;
+
+	lenstr = ft_strlen(s1);
+	i = trim_begin(s1, set);
+	s1 = s1 + i;
+	while (lenstr - i > 0)
+	{
+		if (ft_strchr(set, s1[lenstr - i - 1]))
 			lenstr--;
-	    	else
-	    		break;
-    	}
-    //printf("endtrimmed:%zu\n", lenstr-i);
-    	out = (char *)malloc((lenstr-i + 1) * sizeof(char));
-    	if (out == NULL)
-        	return NULL;
-    	ft_strlcpy(out, s1, lenstr-i+1);
-    	return out;
+		else
+			break ;
+	}
+	out = (char *)malloc((lenstr - i + 1) * sizeof(char));
+	if (out == NULL)
+		return (NULL);
+	ft_strlcpy(out, s1, lenstr - i + 1);
+	return (out);
 }
 /*
 int main()
