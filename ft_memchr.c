b4e86cd50359139d6f387603e37d6f6f2f640942
Franck Henckell <fhenckel@c1r6p15.hive.fi>
2024-11-07 16:24:01 +0200

    memchr normi

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: fhenckel <fhenckel@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/28 15:26:41 by fhenckel          #+#    #+#             */
-/*   Updated: 2024/10/31 17:25:56 by fhenckel         ###   ########.fr       */
+/*   Updated: 2024/11/07 15:08:35 by fhenckel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,15 +16,15 @@
 void	*ft_memchr(const void *s, int c, size_t n)
 {
 	unsigned char	*p;
-	unsigned char uc;
-	size_t	i;
+	unsigned char	uc;
+	size_t			i;
 
 	p = (unsigned char *)s;
 	uc = (unsigned char)c;
 	i = 0;
 	while (i < n)
 		if (p[i++] == uc)
-			return (void *)(p + i - 1);
+			return ((void *)(p + i - 1));
 	return (NULL);
 }
 /*
