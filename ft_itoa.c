2df6f428fd10f94da282560fe88effa777c5dd27
GHenckell <greytho2805@gmail.com>
2024-11-07 23:41:20 +0200

    itoa fixed

--- ft_itoa.c
+++ ft_itoa.c
@@ -11,23 +11,46 @@
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdio.h>
+//#include <stdio.h>
+
+static int	integer_length(int n)
+{
+	int len;
+	len = 0;
+	if (n == INT_MIN)
+		return (11);
+	if (!n)
+		return (1);
+	if (n<0)
+	{
+		len++;
+		n *= -1;
+	}
+	while (n > 0)
+        {
+		len++;
+                n = n / 10;
+        }
+	return (len);
+}
 
 static	void	ft_putnbr(int n, char *out)
 {
 	int	mod;
 	int	idx;
+	int	sign;
 
 	idx = 0;
+	sign = 0;
 	if (n == INT_MIN)
 	{
-		ft_strlcpy(out, "-2147483648", 11);
+		ft_strlcpy(out, "8463847412-", 12);
 		return ;
 	}
 	if (n < 0)
 	{
-		out[idx++] = '-';
-		n = n * (-1);
+		sign = 1;
+		n *= -1;
 	}
 	while (n > 0)
 	{
@@ -35,40 +58,47 @@ static	void	ft_putnbr(int n, char *out)
 		n = n / 10;
 		out[idx++] = mod + 48;
 	}
+	if (sign)
+		out[idx++] = '-';
 	out[idx++] = '\0';
 }
 
 char	*ft_itoa(int n)
 {
 	char	*out;
-	char	*temp;
-	int	len;
-	int	idx;
+	size_t	end;
+	size_t	start;
+	char 	temp;
+	int	size;
 
-	out = (char *)malloc(12 * sizeof(char));
+	size = integer_length(n);
+	out = (char *)malloc(size+1 * sizeof(char));
 	if (out == NULL)
 		return (NULL);
-	ft_putnbr(n, out);
-	temp = out;
-	idx = 0;
-	if (temp[0] == '-')
+	ft_memset(out,0,size);
+	if (n*-1 == 0)
 	{
-		out[idx++] = '-';
-		len = ft_strlen(temp+1);
-		printf("%d\n",len);
+		out[0] = '0';
+		out[1] = '\0';
+		return out;
 	}
-	else
-		len = ft_strlen(temp);
-	while ( len > 0)
+	ft_putnbr(n, out);
+	start = 0;
+	end = ft_strlen(out)-1;
+	while (start < end)
 	{
-		out[idx++] = temp[len];
-		len--;	
+		temp = out[start];
+		out[start] = out[end]; 
+		out[end] = temp;
+		start++;
+		end--;
 	}
 	return (out);
 }
-
+/*
 int main()
 {
-	char *s = ft_itoa(-623);
+	char *s = ft_itoa(0);
 	printf("%s\n",s);
-}
+	free(s);
+}*/
