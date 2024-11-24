4e3df902ba15befbb124b2a3825e482d9e3b967c
GHenckell <greytho2805@gmail.com>
2024-11-24 12:53:26 +0200

    improve no new line

--- mygetnetxline/get_next_line.c
+++ mygetnetxline/get_next_line.c
@@ -1,29 +1,23 @@
 #include "get_next_line.h"
 #include <stdio.h>
 
-void	create_line(char **ptrline, int fd)
+void create_line(char **ptrline, int fd, int *bytes_read)
 {
-	char	*temp;
-	char	*buff_content;
-	int		bytes_read;
+	char *temp;
+	char *buff_content;
 
-	temp = NULL;
-	buff_content = NULL;
-	bytes_read = 0;
-	while (ft_strchr(*ptrline, '\n') == NULL)
+	while (*bytes_read > 0 && ft_strchr(*ptrline, '\n') == NULL)
 	{
 		buff_content = malloc((BUFFER_SIZE + 1));
 		if (!buff_content)
-			return ;
-		bytes_read = read(fd, buff_content, BUFFER_SIZE);
-		if (bytes_read <= 0)
+			return;
+		*bytes_read = read(fd, buff_content, BUFFER_SIZE);
+		if (*bytes_read == -1)
 		{
 			free(buff_content);
-			free(*ptrline);
-			*ptrline = NULL;
-			return ;
+			return;
 		}
-		buff_content[bytes_read] = '\0';
+		buff_content[*bytes_read] = '\0';
 		temp = ft_strjoin(*ptrline, buff_content);
 		free(*ptrline);
 		free(buff_content);
@@ -31,61 +25,83 @@ void	create_line(char **ptrline, int fd)
 	}
 }
 
-char	*allocate_line(char **headline)
+char *allocate_line(char **headline)
 {
-	char	*temp;
-	char	*out;
-	size_t	rem_len;
+	char *temp;
+	char *out;
+	size_t rem_len;
+	char *ptrline;
 
-	rem_len = ft_strlen(ft_strchr(*headline, '\n') + 1);
-	out = malloc(((ft_strlen(*headline) - rem_len) + 1) * sizeof(char));
+	ptrline = ft_strchr(*headline, '\n');
+	rem_len = ptrline - *headline + 1;
+	out = malloc((rem_len + 1) * sizeof(char));
 	if (!out)
 		return (NULL);
-	ft_strlcpy(out, *headline, ft_strlen(*headline) - rem_len+1);
-	temp = malloc((rem_len + 1) * sizeof(char));
-	if (temp)
-		ft_strlcpy(temp, ft_strchr(*headline, '\n') + 1, rem_len+1);
+	ft_strlcpy(out, *headline, rem_len + 1);
+	temp = ft_strdup(ptrline + 1);
+	if (!temp)
+	{
+		free(out);
+		return (NULL);
+	}
 	free(*headline);
 	*headline = temp;
 	return (out);
 }
-char	*get_next_line(int fd)
+
+char *allocate_noline(char **headline)
 {
-	static char	*headline;
-	char		*out;
+	char *out;
+	out = ft_strdup(*headline);
+	free(*headline);
+	*headline = NULL;
+	return (out);
+}
+char *get_next_line(int fd)
+{
+	static char *headline;
+	char *out;
+	int bytes_read = 1;
 
 	out = NULL;
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
 	if (!headline)
 	{
 		headline = malloc(1);
+		if (!headline)
+			return (NULL);
 		headline[0] = '\0';
 	}
-	if (!headline)
+	create_line(&headline, fd, &bytes_read);
+	if (!headline[0])
+	{
+		free(headline);
+		headline = NULL;
 		return (NULL);
-	if (fd < 0 || BUFFER_SIZE <= 0)
-		return (NULL);
-	create_line(&headline, fd);
-	if (headline == 0)
-		return (NULL);
+	}
 	if (ft_strchr(headline, '\n'))
 		out = allocate_line(&headline);
+	else
+		out = allocate_noline(&headline);
 	return (out);
 }
-int	main(void)
+/*
+int main(void)
 {
 	char *line;
-	int fd = open("no_nl.txt", O_RDWR);
+	int fd = open("test1.txt", O_RDWR);
 
 	while ((1))
 	{
 		line = get_next_line(fd);
-		printf("->nl:%s", line);
+		printf("%s", line);
 		if (line)
 			free(line);
 		if (!line)
-			break ;
+			break;
 	}
 
 	close(fd);
 	return (0);
-}
+}*/
--- mygetnetxline/get_next_line.h
+++ mygetnetxline/get_next_line.h
@@ -11,26 +11,27 @@
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
-# define GET_NEXT_LINE_H
+#define GET_NEXT_LINE_H
 
-# ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 4096
-# endif
+#ifndef BUFFER_SIZE
+#define BUFFER_SIZE 4096
+#endif
 
-# if BUFFER_SIZE > 10000000
-#  undef BUFFER_SIZE
-#  define BUFFER_SIZE 4096
-# endif
+#if BUFFER_SIZE > 10000000
+#undef BUFFER_SIZE
+#define BUFFER_SIZE 1024
+#endif
 
-# include <fcntl.h>
-# include <stdlib.h>
-# include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <unistd.h>
 
-char	*ft_strchr(const char *s, int c);
-char	*ft_strjoin(char const *s1, char const *s2);
-size_t	ft_strlen(const char *s);
-size_t	ft_strlcpy(char *dst, const char *src, size_t size);
-size_t	ft_strlen(const char *s);
-char	*get_next_line(int fd);
+char *ft_strchr(const char *s, int c);
+char *ft_strjoin(char const *s1, char const *s2);
+size_t ft_strlen(const char *s);
+size_t ft_strlcpy(char *dst, const char *src, size_t size);
+size_t ft_strlen(const char *s);
+char *ft_strdup(char *src);
+char *get_next_line(int fd);
 
 #endif
