4dadb819ef9c141b223d3392231cc36c3b0f4832
Franck Henckell <fhenckel@c1r5p10.hive.fi>
2024-11-30 15:05:49 +0200

    main to test -1

--- myprintf/MAIN.c
+++ myprintf/MAIN.c
@@ -3,7 +3,7 @@
 
 int	main(void)
 {
-	int	r1;
+	/*int	r1;
 	int	r2;
 
 	r1 = ft_printf("pointer: %p dec:%%%d, X: %X, h:%x, str:%s\n",
@@ -11,6 +11,13 @@ int	main(void)
 	r2 = printf("pointer: %p dec:%%%d, X: %X, h:%x, str:%s\n", (void *)LONG_MIN,
 			-100, -1988, 56645646, "welcome!!!");
 	printf("fprint:%d vs rpprinnt:%d\n", r1, r2);
+	*/
+
+	int orig_stdout = dup(STDOUT_FILENO);
+	fclose(stdout);
+	int ret = printf("aaaaaaaaaa%d",10);
+	stdout = (FILE *)fdopen(orig_stdout, "w");
+	printf("%d\n", ret);
 
 }
-//cc ft_printf.c -I. -L. -lftprintf -o outmain.o
+//cc MAIN.c -I. -L. -lftprintf -o outmain.o
